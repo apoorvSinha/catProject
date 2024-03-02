12721c24c0045ed8f0db329c5e00c9c9b9cf45c6
apoorvSinha <apoorvsinha96@gmail.com>
2024-03-02 17:33:12 +0530

    format

--- src/test/java/stepDefinitions/favStepDefinitions.java
+++ src/test/java/stepDefinitions/favStepDefinitions.java
@@ -9,6 +9,5 @@ public class favStepDefinitions extends CatUtils {
     @Given("user {string} their favourite cat ID")
     public void user_their_favourite_cat_ID(String httpMethod) {
 
-        }
     }
 }
