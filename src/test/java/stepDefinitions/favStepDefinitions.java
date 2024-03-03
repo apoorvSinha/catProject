e5a4f2c6f8f7d99b6c90cd21772aaabe039d65bd
apoorvSinha <apoorvsinha96@gmail.com>
2024-03-03 17:51:51 +0530

    Setting up my favourite cat - next step is to deserialize

--- src/test/java/stepDefinitions/favStepDefinitions.java
+++ src/test/java/stepDefinitions/favStepDefinitions.java
@@ -1,18 +1,30 @@
 package stepDefinitions;
 
 import io.cucumber.java.en.*;
-import io.restassured.specification.RequestSpecification;
-import pojo.Root;
+import io.restassured.http.ContentType;
 import utils.CatUtils;
 
 import java.util.HashMap;
 
+import static io.restassured.RestAssured.*;
+
 public class favStepDefinitions extends CatUtils {
+    private static final String favouriteEndpoint = "/v1/favourites";
+
+    private String getFavIdOfCats() {
+        return objects.get(objects.size() - 1).getId();
+    }
 
     @Given("user {string} their favourite cat ID")
     public void user_their_favourite_cat_ID(String httpMethod) {
-        for(Root object: objects){
-            System.out.println(object.getId());
+        map = new HashMap<>();
+        requestSpecification = setRequestSpecification(map, ContentType.JSON.toString());
+        responseSpecification = setResponseSpecification(httpMethod, ContentType.JSON.toString());
+        requestSpecBuilder = given().spec(requestSpecification).log().all();
+        if (httpMethod.equalsIgnoreCase("GET")) {
+            response = requestSpecBuilder.when().get(favouriteEndpoint)
+                    .then().log().all().spec(responseSpecification)
+                    .extract().response();
         }
     }
 }
